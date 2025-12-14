31b856ed461057a3d9cc55157246792d2cb94407
DeepanshiJainAstrovaani <astrovaani60@gmail.com>
2025-12-14 22:54:40 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className={styles.headerBg}>
       <div className={styles.headerContainer}>
-        <img src={logoDark} alt="Astrovaani Logo" style={{ width: '10rem', height: 'auto' }} />
+        <img src={logoDark} alt="Astrovaani Logo" style={{ width: '9rem', height: 'auto' }} />
       </div>
     </header>
   );
